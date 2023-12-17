e7de1d057b116f5052f2a718d843da884eaf81e1
AdoDeveloper <105835711+AdoDeveloper@users.noreply.github.com>
2023-12-17 12:22:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,50 @@
+function updateChristmasDate() {
+  const today = new Date();
+  const christmas = new Date(today.getFullYear(), 11, 25); // 11 representa diciembre en JavaScript
 
-// Obtener la fecha actual
-const today = new Date();
+  // Si la fecha actual es después de Navidad, obtener la fecha de Navidad del próximo año
+  if (today.getMonth() === 11 && today.getDate() > 25) {
+    christmas.setFullYear(christmas.getFullYear() + 1);
+  }
 
-// Obtener la fecha de Navidad (25 de diciembre)
-const christmas = new Date(2023, 12, 25);
-
-// Si la fecha actual es después de Navidad, obtener la fecha de Navidad del próximo año
-if (today.getMonth() == 12 && today.getDate() <= 25) {
-  christmas.setFullYear(christmas.getFullYear() + 1);
+  return christmas;
 }
 
-// Función para actualizar el reloj
 function updateClock() {
-  // Obtener la diferencia entre la fecha actual y la fecha de Navidad
+  const today = new Date();
+  const christmas = updateChristmasDate();
+
   const diff = christmas.getTime() - today.getTime();
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-  // Calcular los días, horas, minutos y segundos que faltan
-  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
-
-  // Actualizar el contenido de los elementos HTML
-  document.getElementById("days").innerHTML = days;
-  document.getElementById("hours").innerHTML = hours;
-  document.getElementById("minutes").innerHTML = minutes;
-  document.getElementById("seconds").innerHTML = seconds;
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
 }
-
-// Llamar a la función updateClock() cada segundo
-setInterval(updateClock, 1000);
 
 function countdown() {
-    const now = new Date();
-    const christmas = new Date(2023, 12, 25);
-    if (now.getMonth() == 12 && now.getDate() <= 25) {
-      christmas.setFullYear(christmas.getFullYear() + 1);
-    }
-    const difference = christmas.getTime() - now.getTime();
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-  
-    document.getElementById("days").innerText = days;
-    document.getElementById("hours").innerText = hours;
-    document.getElementById("minutes").innerText = minutes;
-    document.getElementById("seconds").innerText = seconds;
-  }
-  
-  countdown();
-  setInterval(countdown, 1000);  
+  const today = new Date();
+  const christmas = updateChristmasDate();
+
+  const difference = christmas.getTime() - today.getTime();
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+}
+
+// Llamar a la función updateClock() y countdown() al inicio
+updateClock();
+setInterval(updateClock, 1000);
+
+countdown();
+setInterval(countdown, 1000);
